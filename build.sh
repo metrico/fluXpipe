ed335c69f26c63526046d2f614e682886ab6da42
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-01-09 23:52:14 +0100

    Update build.sh
    Signed-off-by: Lorenzo Mangani <lorenzo.mangani@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 export PKG_CONFIG_PATH=$(pwd)
 
-#echo "Building fluxpipe ..."
-#go build -a -ldflags '-extldflags "-static -w -ldl"' -o fluxpipe fluxpipe.go
 echo "Building fluxpipe-server ..."
 go build -a -ldflags '-extldflags "-static -w -ldl"' -o fluxpipe-server fluxpipe-server.go
